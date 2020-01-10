8fdfacad90d32f69e024bb52b71986ce27ec5461
Sabbir <SabbirMac@labs-mac.local>
2020-01-10 19:41:58 +0600

    Demo Collectionniez with random picture

---
+++ CollectionView/DataCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  DataCollectionViewCell.swift
+//  CollectionView
+//
+//  Created by Sabbir on 1/10/20.
+//  Copyright © 2020 Sabbir. All rights reserved.
+//
+
+import UIKit
+
+class DataCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var img: UIImageView!
+    @IBOutlet weak var lbl: UILabel!
+    
+}
--- CollectionView/ViewController.swift
+++ CollectionView/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var collectionview: UICollectionView!
+    
+
+    var imgArray = ["Welcome","New Technology","Sabbir Hossain","Demo","Collectionview"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,3 +24,17 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UICollectionViewDataSource,UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return imgArray.count
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        var cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? DataCollectionViewCell
+        cell?.img.image = UIImage(named: imgArray[indexPath.row])
+        cell?.lbl.text = imgArray [indexPath.row]
+        return cell!
+    }
+}
